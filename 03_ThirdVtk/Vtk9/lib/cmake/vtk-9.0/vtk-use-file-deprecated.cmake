b097d3121f60363b65f9566eb56fd9a2423682c0
张建伟 <Jianwei1992@foxmail.com>
2021-08-04 17:02:03 +0800

    整理无用文件
    整理无用文件

--- 03_ThirdVtk/Vtk9/lib/cmake/vtk-9.0/vtk-use-file-deprecated.cmake
+++
@@ -1,2 +0,0 @@
-message(DEPRECATION
-  "The `VTK_USE_FILE` is no longer used starting with 8.90.")
--- 03_ThirdVtk/Vtk9/lib/cmake/vtk-9.0/vtk-use-file-error.cmake
+++
@@ -1,4 +0,0 @@
-message(FATAL_ERROR
-  "The `VTK_USE_FILE` is no longer used starting with 8.90. The "
-  "`include(\${VTK_USE_FILE})` line is no longer necessary and can just be "
-  "removed.")
