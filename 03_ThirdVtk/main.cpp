c200a3ce1a3280bb8c8cfe4ccedac956f567ab1a
张建伟 <Jianwei1992@foxmail.com>
2021-08-13 09:43:15 +0800

    开启 显卡加速，不确定是否正确，帧数提升不明显

--- 03_ThirdVtk/main.cpp
+++ 03_ThirdVtk/main.cpp
@@ -11,6 +11,9 @@ VTK_MODULE_INIT(vtkRenderingContextOpenGL2);
 int main(int argc, char *argv[])
 {
     //    vtkOutputWindow::SetGlobalWarningDisplay(0);                              //关闭错误输出
+
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);                     //使用显卡加速 ？？？ 不确定是否正确，帧数提升不明显
+
     QApplication a(argc, argv);
     a.setStyleSheet("QTabWidget::pane{border-style:outset;background:transparent;}"
                     "QMenuBar{background-color:rgb(240,240,240);}"
